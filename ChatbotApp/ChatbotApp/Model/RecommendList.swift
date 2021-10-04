e54e9d21bae004b5cc5b6166cacb2bfb3b847921
yeon <jryoun0404@gmail.com>
2021-10-04 11:40:20 +0900

    feat: Create RecommendList model

--- ChatbotApp/ChatbotApp/Model/RecommendList.swift
+++ ChatbotApp/ChatbotApp/Model/RecommendList.swift
@@ -1,8 +1,13 @@
 //
-//  Recommend.swift
+//  RecommendList.swift
 //  ChatbotApp
 //
 //  Created by Yeon on 2021/10/04.
 //
 
 import Foundation
+
+struct RecommendList: Decodable {
+    let page: Int
+    let recommends: [Recommend]
+}
