04c87a96f94ea335ce12a5a2d21282a2706b1019
yeon <jryoun0404@gmail.com>
2021-10-04 11:44:12 +0900

    feat: Create Banners model

---
+++ ChatbotApp/ChatbotApp/Model/Banners.swift
@@ -0,0 +1,13 @@
+//
+//  Banners.swift
+//  ChatbotApp
+//
+//  Created by Yeon on 2021/10/04.
+//
+
+import Foundation
+
+struct Banners: Decodable {
+    let imagURLs: [String]
+    let redirectURLs: [String]
+}
