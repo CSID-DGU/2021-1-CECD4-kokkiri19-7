94340d69c51f269cae240cef07140f1cb9076eab
yeon <jryoun0404@gmail.com>
2021-10-07 20:34:36 +0900

    Create RecommendDetailViewController file

---
+++ ChatbotApp/ChatbotApp/ViewControllers/RecommendDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RecommendDetailViewController.swift
+//  ChatbotApp
+//
+//  Created by Yeon on 2021/10/07.
+//
+
+import UIKit
+
+final class RecommendDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
