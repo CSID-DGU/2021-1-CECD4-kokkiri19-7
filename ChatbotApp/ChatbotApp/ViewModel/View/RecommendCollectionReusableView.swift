4cae9e67bd8fc1f11c97b74f0d1bbc48014c61cf
yeon <jryoun0404@gmail.com>
2021-10-07 13:21:31 +0900

    Create RecommendCollectionReusableView file

---
+++ ChatbotApp/ChatbotApp/ViewModel/View/RecommendCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  RecommendCollectionReusableView.swift
+//  ChatbotApp
+//
+//  Created by Yeon on 2021/10/07.
+//
+
+import UIKit
+
+final class RecommendCollectionReusableView: UICollectionReusableView {
+        
+}
