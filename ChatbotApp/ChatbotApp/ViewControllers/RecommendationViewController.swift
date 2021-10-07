ea3932c9ac9f28e8b28ff573526acb9b58571350
yeon <jryoun0404@gmail.com>
2021-10-07 13:43:24 +0900

    feat: Add recommendCollectionView property

--- ChatbotApp/ChatbotApp/ViewControllers/RecommendationViewController.swift
+++ ChatbotApp/ChatbotApp/ViewControllers/RecommendationViewController.swift
@@ -11,6 +11,15 @@ final class RecommendationViewController: UIViewController {
     static let identifier = "RecommendationViewController"
     private var viewModel = RecommendListViewModel()
 
+    private var recommendCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        let nib = UINib(nibName: RecommendCollectionViewCell.identifier, bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: RecommendCollectionViewCell.identifier)
+        collectionView.register(RecommendCollectionReusableView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionFooter, withReuseIdentifier: RecommendCollectionReusableView.identifier)
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
