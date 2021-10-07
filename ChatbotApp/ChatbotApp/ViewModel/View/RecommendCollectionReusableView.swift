ef8c574859e4acc2ac00f65cabdd989aafb4d8cc
yeon <jryoun0404@gmail.com>
2021-10-07 13:29:41 +0900

    feat: Add loadingIndicator property

--- ChatbotApp/ChatbotApp/ViewModel/View/RecommendCollectionReusableView.swift
+++ ChatbotApp/ChatbotApp/ViewModel/View/RecommendCollectionReusableView.swift
@@ -9,4 +9,11 @@ import UIKit
 
 final class RecommendCollectionReusableView: UICollectionReusableView {
     static let identifier = "RecommendCollectionReusableView"
+    private var loadingIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.color = .systemGray
+        indicator.style = .medium
+        return indicator
+    }()
 }
