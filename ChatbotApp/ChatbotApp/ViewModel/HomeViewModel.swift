d7e00b7765c7f2866237eabd68cbc04ff493ee39
yeon <jryoun0404@gmail.com>
2021-10-08 00:21:42 +0900

    feat: Add test method

--- ChatbotApp/ChatbotApp/ViewModel/HomeViewModel.swift
+++ ChatbotApp/ChatbotApp/ViewModel/HomeViewModel.swift
@@ -81,6 +81,22 @@ final class HomeViewModel {
             self?.bannerImages.value?.append(image)
         }
     }
+    
+    func test() {
+        DispatchQueue.main.asyncAfter(deadline: .now()+3.0) {
+            var images = [UIImage]()
+            for name in imagesExample {
+                images.append(UIImage(named: name)!)
+            }
+            
+            self.bannerImages.value = images
+            self.characterImage.value = UIImage(named: "Character")
+            self.nicknameLabelText.value = "연정민님 안녕하세요"
+            self.locationLabelText.value = "서울시 중구청 새로운 소식입니다"
+            
+            self.urls.value = ["https://www.google.co.kr", "https://www.naver.com", "https://jryoun1.github.io", "https://www.junggu.seoul.kr/html/jg4u_2109.html"]
+        }
+    }
 }
 
 extension HomeViewModel {
