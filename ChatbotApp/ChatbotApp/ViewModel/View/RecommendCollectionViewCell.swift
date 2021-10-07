cf4fd4647efc86f0b97fec76996eb5d82f7303c8
yeon <jryoun0404@gmail.com>
2021-10-07 12:52:32 +0900

    Create RecommendCollectionViewCell and xib file

---
+++ ChatbotApp/ChatbotApp/ViewModel/View/RecommendCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RecommendCollectionViewCell.swift
+//  ChatbotApp
+//
+//  Created by Yeon on 2021/10/07.
+//
+
+import UIKit
+
+final class RecommendCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
