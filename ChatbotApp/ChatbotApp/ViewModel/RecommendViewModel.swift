04d0acea47f25459031147eb53d0ac1d2867b92e
yeon <jryoun0404@gmail.com>
2021-10-07 12:37:21 +0900

    feat: Add properties in RecommendListViewModel

--- ChatbotApp/ChatbotApp/ViewModel/RecommendViewModel.swift
+++ ChatbotApp/ChatbotApp/ViewModel/RecommendViewModel.swift
@@ -8,7 +8,11 @@
 import Foundation
 
 final class RecommendViewModel {
-    
+    private var apiRequestLoader: APIRequestLoader<GetRecommendListAPIRequest>!
+    var currentPage: Int = 1
+    var isPaging: Bool = false
+    var hasNextPage: Bool = false
+    var recommendList: Observable<[Recommend]> = Observable([])
 }
 
 extension RecommendViewModel {
