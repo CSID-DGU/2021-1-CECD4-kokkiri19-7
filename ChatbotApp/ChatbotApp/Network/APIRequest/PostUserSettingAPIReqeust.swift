c687576066738e31cafa512a6744c6a0a2d95951
yeon <jryoun0404@gmail.com>
2021-10-07 12:14:40 +0900

    feat: Create PostUserSettingAPIRequest model

--- ChatbotApp/ChatbotApp/Network/APIRequest/PostUserSettingAPIReqeust.swift
+++ ChatbotApp/ChatbotApp/Network/APIRequest/PostUserSettingAPIReqeust.swift
@@ -1,8 +1,23 @@
 //
-//  PostUserAPIReqeust.swift
+//  PostUserSettingAPIReqeust.swift
 //  ChatbotApp
 //
 //  Created by Yeon on 2021/10/07.
 //
 
 import Foundation
+
+struct PostUserSettingAPIReqeust: APIRequest {
+    func makeRequest(from user: UserSetting) throws -> URLRequest {
+        guard var components = URLComponents(string: ChatbotAPI.baseURL) else {
+            throw ChatbotError.failToMakeURL
+        }
+        components.path += "users/new"
+        
+        return URLRequest(url: components.url!)
+    }
+    
+    func parseResponse(data: Data) throws {
+        return
+    }
+}
