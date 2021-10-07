d162d4f7b9abe89807aea0eaeeaad254e67c8375
yeon <jryoun0404@gmail.com>
2021-10-07 12:38:34 +0900

    feat: Add fetch method in RecommendListViewModel

--- ChatbotApp/ChatbotApp/ViewModel/RecommendListViewModel.swift
+++ ChatbotApp/ChatbotApp/ViewModel/RecommendListViewModel.swift
@@ -13,6 +13,33 @@ final class RecommendListViewModel {
     var isPaging: Bool = false
     var hasNextPage: Bool = false
     var recommendList: Observable<[Recommend]> = Observable([])
+    
+    func fetch() {
+        let getRecommendListAPIRequest = GetRecommendListAPIRequest()
+        apiRequestLoader = APIRequestLoader(apiReqeust: getRecommendListAPIRequest)
+        
+        guard let email = User.shared.email,
+              let nickname = User.shared.nickname else {
+                  return
+              }
+        let userIdentifier = UserIdentifer(email: email, nickname: nickname)
+        
+        apiRequestLoader.loadAPIReqeust(requestData: userIdentifier) { [weak self] recommendList, error in
+            if let error = error {
+                print(error)
+            }
+            
+            guard let recommendList = recommendList, recommendList.recommends.count > 0 else {
+                self?.hasNextPage = false
+                return
+            }
+            
+            self?.hasNextPage = true
+            _ = recommendList.recommends.compactMap({ recommend in
+                self?.recommendList.value?.append(recommend)
+            })
+        }
+    }
 }
 
 extension RecommendListViewModel {
