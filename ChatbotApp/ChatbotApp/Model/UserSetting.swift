17a23f4a1edb155a239f8b464bf676707ca6e0ed
yeon <jryoun0404@gmail.com>
2021-10-07 12:08:42 +0900

    feat: Create UserSetting model

--- ChatbotApp/ChatbotApp/Model/UserSetting.swift
+++ ChatbotApp/ChatbotApp/Model/UserSetting.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+struct UserSetting: Codable {
+    let email: String
+    let nickname: String
+    let gender: String
+    let age: Int
+    let birthday: Date
+    let city: String
+    let province: String
+}
