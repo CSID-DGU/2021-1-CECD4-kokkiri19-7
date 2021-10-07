6b7da8c22f5eb07cb16564a0a8475d441fd96347
yeon <jryoun0404@gmail.com>
2021-10-07 23:32:24 +0900

    feat: Add IBOutlet properties

--- ChatbotApp/ChatbotApp/ViewControllers/RecommendDetailViewController.swift
+++ ChatbotApp/ChatbotApp/ViewControllers/RecommendDetailViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 final class RecommendDetailViewController: UIViewController {
     static let identifier = "RecommendDetailViewController"
     private var recommend: Recommend?
+    @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private weak var contentLabel: UILabel!
+    @IBOutlet private weak var goButton: UIButton!
+    @IBOutlet private weak var navigationTitleItem: UINavigationItem!
+    @IBOutlet private weak var gobackButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
