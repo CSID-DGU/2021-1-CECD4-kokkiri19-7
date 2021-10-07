72f30b2d6e7a4b1b7d04f5576011ef5198be35d6
yeon <jryoun0404@gmail.com>
2021-10-07 13:19:56 +0900

    feat: Add configureCell method

--- ChatbotApp/ChatbotApp/ViewModel/View/RecommendCollectionViewCell.swift
+++ ChatbotApp/ChatbotApp/ViewModel/View/RecommendCollectionViewCell.swift
@@ -13,9 +13,28 @@ final class RecommendCollectionViewCell: UICollectionViewCell {
         static let policy = "정책"
         static let welfare = "복지"
     }
-
+    
     static let identifier = "RecommendCollectionViewCell"
     @IBOutlet private var titleLabel: UILabel!
     @IBOutlet private var classificationLabel: UILabel!
     @IBOutlet private var contentLabel: UILabel!
+    
+    func configureCell(with recommend: Recommend) {
+        classificationLabel.text = "[\(recommend.classification)]"
+        switch recommend.classification {
+        case Classification.tour:
+            classificationLabel.textColor = .systemOrange
+        case Classification.policy:
+            classificationLabel.textColor = .systemRed
+        case Classification.welfare:
+            classificationLabel.textColor = .systemBlue
+        default:
+            classificationLabel.textColor = .black
+        }
+        
+        titleLabel.text = "제목: \(recommend.title)"
+        titleLabel.font = .preferredFont(forTextStyle: .headline)
+        
+        contentLabel.text = "내용: \(recommend.content)"
+    }
 }
